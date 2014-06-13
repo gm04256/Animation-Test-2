3cfb067b9fe517bb8b318f928ebc2ed738f7a95b
Ma Yan <896098846@qq.com>
2014-06-13 19:11:36 +0900

    Project created

---
+++ Animation Test 2/Animation Test 2/BGMViewController.h
@@ -0,0 +1,13 @@
+//
+//  BGMViewController.h
+//  Animation Test 2
+//
+//  Created by 馬 岩 on 14-6-13.
+//  Copyright (c) 2014年 馬 岩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGMViewController : UIViewController
+
+@end
